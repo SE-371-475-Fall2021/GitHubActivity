c4c6fa41538b89e3a181faf79e11e384c9270432
HJM-Depaul <bjhj0230@gmail.com>
2021-09-24 01:17:41 -0500

    Hyukjun Moon changes to TestApp.java

--- src/main/java/com/gsd/TestApp.java
+++ src/main/java/com/gsd/TestApp.java
@@ -7,7 +7,7 @@ public class TestApp {
         System.out.println("SE475/371 GSD Course \n");
 
         //TODO Print your name like this "Name: Vahid Alizadeh"
-        System.out.println("Name: Hyukjun Moon");
+        System.out.println("Name: Hyukjun Moon.");
 
     }
 }
